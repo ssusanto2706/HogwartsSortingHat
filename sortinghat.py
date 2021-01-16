ac6d2b58902e7eb908330fe89cae53767016d980
ssusanto2706 <samantha.susanto@gmail.com>
2021-01-15 19:39:51 -0800

    imports

---
+++ sortinghat.py
@@ -0,0 +1,14 @@
+import torch
+import jovian
+import torchvision
+import torch.nn as nn
+import matplotlib.pyplot as plt
+import torch.nn.functional as F
+import torchvision.transforms as transforms
+from torchvision.datasets import MNIST
+from torch.utils.data import random_split
+from torch.utils.data import DataLoader, TensorDataset
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
